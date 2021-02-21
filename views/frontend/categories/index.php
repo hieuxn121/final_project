5ba1ec66d920f0a0218d1a8576eb69dd0701329c
nguyenhieu12012000@gmail.com <nguyenhieu12012000@gmail.com>
2021-02-21 14:56:12 +0700

    Final Project php 26

--- views/frontend/categories/index.php
+++ views/frontend/categories/index.php
@@ -18,7 +18,13 @@
 						<img src="<?php echo('images/post/'.$value['thumbnail']) ;?>" alt="Image" class="img-responsive">
 					</a>
 				</figure>
-				<span class="fh5co-meta"><a href="index.php?type=frontend&mod=post&act=detail&id=<?php echo $value['id'] ?>"><?php $code = $value['category_id'];echo $categories[$code-1]['name']; ?></a></span>
+				<span class="fh5co-meta">
+					<a href="index.php?type=frontend&mod=post&act=detail&id=<?php echo $value['id'] ?>">
+						<?php $code = $value['category_id']; foreach($categories as $key => $value2){
+							if($value2['id'] == $code) echo $value2['name']; ?>
+						<?php } ?>
+					</a>
+				</span>
 				<h2 class="fh5co-article-title"><a href="index.php?type=frontend&mod=post&act=detail&id=<?php echo $value['id'] ?>"><?php echo $value['title'] ?></a></h2>
 				<span class="fh5co-meta fh5co-date"><?php echo $value['created_at']; ?></span>
 			</article>

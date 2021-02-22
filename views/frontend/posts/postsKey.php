8a4c42d53693c6f78aa7e6afc5bb42fb03904305
nguyenhieu12012000@gmail.com <nguyenhieu12012000@gmail.com>
2021-02-22 20:54:34 +0700

    Final project php 26

--- views/frontend/posts/postsKey.php
+++ views/frontend/posts/postsKey.php
@@ -4,9 +4,10 @@
 	<div class="container-fluid">
 		<div class="row fh5co-post-entry">
 			<h1 style="text-align: center;margin-bottom: 50px;"><a href="">Posts Searched</a></h1>
-			<?php foreach ($posts as $key => $value) {?>
+			<?php if(isset($posts)){foreach ($posts as $key => $value) {?>
 				<?php require('views/frontend/includes/article.php') ?>
-			<?php } ?>
+			<?php }} ?>
+			<?php if (!isset($posts))echo "<h1 style='text-align:center;'>No posts results</h1>"?>
 			<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
 		</div>
 	</div>
